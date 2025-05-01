fcd46561bf394c50a4bdd5d99543cfc6f6267ff0
StiakakisJ <147503845+StiakakisJ@users.noreply.github.com>
2025-05-01 21:33:58 +0300

    Update DDL.sql
    Move the insert statements to dummy_data

--- DDL.sql
+++ DDL.sql
@@ -52,27 +52,3 @@ create table rating
 
 
 
-#Tickets Dummy Data
-/*
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('4689153305038',       'VIP',  '2025-04-28',   '1338.88',  'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('7486968459915',       'BaS',  '2024-01-22',   '0.0',      'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('0853363471597',       'GA',   '2024-04-17',   '74.15',    'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('4248301946246',       'GA',   '2025-04-20',   '299.64',   'BC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('6827238846665',       'VIP',  '2025-06-23',   '2047.48',  'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('5438084046638',       'VIP',  '2025-06-23',   '2249.91',  'NC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('7312740153109',       'GA',   '2025-06-23',   '151.22',   'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('8915326849972',       'VIP',  '2024-04-17',   '1520.25',  'CC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('3921110660756',       'BaS',  '2024-01-22',   '0.0',      'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('6358915376839',       'VIP',  '2025-06-23',   '1072.54',  'NC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('2150055589692',       'GA',   '2024-09-07',   '109.98',   'CC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('8410797247381',       'GA',   '2022-01-05',   '142.41',   'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('9848540584111',       'GA',   '2023-11-23',   '434.7',    'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('5254767543225',       'VIP',  '2024-08-16',   '2453.87',  'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('7615851870686',       'GA',   '2024-12-22',   '118.27',   'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('1354695711015',       'GA',   '2023-04-21',   '499.82',   'BC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('3979733343817',       'BaS',  '2024-12-22',   '0.0',      'DC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('0692084736043',       'VIP',  '2024-09-07',   '1912.55',  'BC');
-INSERT INTO Tickets (EAN13, category, purchase_date, price, payment_method, isActivated) VALUES ('7291832513072',       'GA',   '2024-09-07',   '286.61',   'NC');
-*/
-
-
