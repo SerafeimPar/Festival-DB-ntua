4b10e55eaa8e391f0da8a0b9e573f434b47db383
Christos Papakrivopoulos <32419292+paparakri@users.noreply.github.com>
2025-05-14 02:39:08 +0300

    Added all queries for posterity

--- Pulse Festival Database/sql/Q11.sql
+++ Pulse Festival Database/sql/Q11.sql
@@ -5,23 +5,23 @@ SELECT
     COUNT(DISTINCT e.festival_year) AS participation_count,
     (SELECT COUNT(DISTINCT e2.festival_year) 
      FROM artistband ab2
-     JOIN performance_artistband pa2 ON ab2.id = pa2.artist_band_id
-     JOIN performance p2 ON pa2.performance_id = p2.id
-     JOIN event e2 ON p2.event_id = e2.id
+     LEFT JOIN performance_artistband pa2 ON ab2.id = pa2.artist_band_id
+     LEFT JOIN performance p2 ON pa2.performance_id = p2.id
+     LEFT JOIN event e2 ON p2.event_id = e2.id
      GROUP BY ab2.id
      ORDER BY COUNT(DISTINCT e2.festival_year) DESC
      LIMIT 1) - COUNT(DISTINCT e.festival_year) AS difference_from_max
 FROM 
     artistband ab
-JOIN 
+LEFT JOIN 
     performance_artistband pa ON ab.id = pa.artist_band_id
-JOIN 
+LEFT JOIN 
     performance p ON pa.performance_id = p.id
-JOIN 
+LEFT JOIN 
     event e ON p.event_id = e.id
 GROUP BY 
     ab.id, ab.name, ab.type
 HAVING 
-    difference_from_max >= 5
+    difference_from_max >= -1
 ORDER BY 
     participation_count DESC, ab.name;
